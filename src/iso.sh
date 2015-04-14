84506194decc4e0fa3204cfc51dd06e72007378c
JustBeYou <mihailferaru2000@gmail.com>
2015-04-14 18:50:34 +0300

    Modified project structure and created support for i386 arch.

--- src/iso.sh
+++ src/iso.sh
@@ -1,2 +1,17 @@
-cp kernel/photonos.elf sysroot/boot
-grub-mkrescue -o photonos.iso sysroot
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/photon.elf isodir/boot/photon.elf
+tar --verbose --create --file isodir/boot/photon.initrd --directory=sysroot $(ls sysroot | grep -v boot)
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "photon" {
+	multiboot /boot/photon.elf
+	module /boot/photon.initrd
+}
+EOF
+grub-mkrescue -o photon.iso isodir
--- src/run.sh
+++
@@ -1,2 +0,0 @@
-cd sysroot/boot
-qemu-system-i386 -kernel photonos.elf
